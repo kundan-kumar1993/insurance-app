a70ed18129b4cc748ca53aa872a2102ed18fafbe
Kundan Rai <kundan.rai@evry.com>
2020-04-14 15:06:37 +0530

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../app/App.css';
+import Home from '../dashboard/home'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/menu/navigator.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import DnsRoundedIcon from '@material-ui/icons/DnsRounded';
+import PermMediaOutlinedIcon from '@material-ui/icons/PhotoSizeSelectActual';
+import SettingsEthernetIcon from '@material-ui/icons/SettingsEthernet';
+import SettingsIcon from '@material-ui/icons/Settings';
+import Report from '@material-ui/icons/Report';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
+
+const categories = [
+  {
+    id: 'Case Management',
+    children: [
+      { id: 'Active Missions', icon: <SettingsEthernetIcon />, active: true },
+      { id: 'Archieves', icon: <DnsRoundedIcon /> },
+      { id: 'Summary', icon: <PermMediaOutlinedIcon /> },
+    ],
+  },
+  {
+    id: 'Settings',
+    children: [
+      { id: 'Profile', icon: <SettingsIcon /> },
+      { id: 'Business/Supervisor', icon: <SettingsEthernetIcon /> },
+      { id: 'Report', icon: <Report /> },
+      { id: 'Insurance Information', icon: <AnnouncementIcon /> },
+      { id: 'News Releases', icon: <AnnouncementIcon /> },
+    ],
+  },
+];
+
+const styles = (theme) => ({
+  categoryHeader: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  categoryHeaderPrimary: {
+    color: theme.palette.common.white,
+  },
+  item: {
+    paddingTop: 1,
+    paddingBottom: 1,
+    color: 'rgba(255, 255, 255, 0.7)',
+    '&:hover,&:focus': {
+      backgroundColor: 'rgba(255, 255, 255, 0.08)',
+    },
+  },
+  itemCategory: {
+    backgroundColor: '#232f3e',
+    boxShadow: '0 -1px 0 #404854 inset',
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  firebase: {
+    fontSize: 24,
+    color: theme.palette.common.white,
+  },
+  itemActiveItem: {
+    color: '#4fc3f7',
+  },
+  itemPrimary: {
+    fontSize: 'inherit',
+  },
+  itemIcon: {
+    minWidth: 'auto',
+    marginRight: theme.spacing(2),
+  },
+  divider: {
+    marginTop: theme.spacing(2),
+  },
+});
+
+function Navigator(props) {
+  const { classes, ...other } = props;
+
+  return (
+    <Drawer variant="permanent" {...other}>
+      <List disablePadding>
+        <ListItem className={clsx(classes.firebase, classes.item, classes.itemCategory)}>
+          Belron Solutions
+        </ListItem>
+        <ListItem className={clsx(classes.item, classes.itemCategory)}>
+          <ListItemIcon className={classes.itemIcon}>
+            <AddCircleOutlineIcon />
+          </ListItemIcon>
+          <ListItemText
+            classes={{
+              primary: classes.itemPrimary,
+            }}
+          >
+            New Mission
+          </ListItemText>
+        </ListItem>
+        {categories.map(({ id, children }) => (
+          <React.Fragment key={id}>
+            <ListItem className={classes.categoryHeader}>
+              <ListItemText
+                classes={{
+                  primary: classes.categoryHeaderPrimary,
+                }}
+              >
+                {id}
+              </ListItemText>
+            </ListItem>
+            {children.map(({ id: childId, icon, active }) => (
+              <ListItem
+                key={childId}
+                button
+                className={clsx(classes.item, active && classes.itemActiveItem)}
+              >
+                <ListItemIcon className={classes.itemIcon}>{icon}</ListItemIcon>
+                <ListItemText
+                  classes={{
+                    primary: classes.itemPrimary,
+                  }}
+                >
+                  {childId}
+                </ListItemText>
+              </ListItem>
+            ))}
+
+            <Divider className={classes.divider} />
+          </React.Fragment>
+        ))}
+      </List>
+    </Drawer>
+  );
+}
+
+Navigator.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navigator);
+
